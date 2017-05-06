ac82a4644ca34a83e4a2f580503077e5b99e5daa
Hugo Benício <hbobenicio@gmail.com>
2017-05-05 22:29:21 -0300

    19. Pattern Matching Continued - Implementing the load function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -30,5 +30,14 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "Something went wrong."
+    end
+  end
   
 end
