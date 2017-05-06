b52f8af7b9f0a224236bd0d438ba5b509b477194
Hugo Benício <hbobenicio@gmail.com>
2017-05-05 23:59:13 -0300

    28. Writting Effective Doctests - Adding a doctest for Cards.contains?/2

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards, except: [:cards, deal: 2]
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  # test "the truth" do
+  #   assert 1 + 1 == 2
+  # end
 end
