2ecfff5e961959792405479779f68a7cd04a5ac5
Hugo Benício <hbobenicio@gmail.com>
2017-05-12 08:26:13 -0300

    70. Model Changesets and Validations

--- discuss/web/models/topic.ex
+++ discuss/web/models/topic.ex
@@ -4,5 +4,11 @@ defmodule Discuss.Topic do
   schema "topics" do
     field :title, :string
   end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
   
 end
