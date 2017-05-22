4db9bb19ae1efc551cf02332ba7d9c26efba3d52
Hugo Benício <hbobenicio@gmail.com>
2017-05-22 07:30:53 -0300

    95. Creating a Github OAuth App - Configuring Ueberauth library

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -22,6 +22,18 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Configures the Überauth (OAuth Library)
+# @see https://github.com/ueberauth/ueberauth
+config :ueberauth, Ueberauth,
+  providers: [
+    # facebook: { Ueberauth.Strategy.Facebook, [ opt1: "value", opts2: "value" ] },
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("OAUTH_DISCUSS_GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("OAUTH_DISCUSS_GITHUB_CLIENT_SECRET")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
